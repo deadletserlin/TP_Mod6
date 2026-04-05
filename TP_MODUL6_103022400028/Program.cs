31853cf0148aba6ab143b471f7404c5dd6825513
Nadhifah Eka C <nadhifaerkacs@gmail.com>
2026-04-05 21:29:50 +0700

    menambahkan class SayaMusicTrack

--- TP_MODUL6_103022400028/Program.cs
+++ TP_MODUL6_103022400028/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using TP_MODUL6_103022400028;
+
+SayaMusicTrack lagu = new SayaMusicTrack("Blue");
+lagu.IncreasePlayCount(3);
+lagu.PrintTrackDetails();
---
+++ TP_MODUL6_103022400028/SayaMusicTrack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400028
+{
+    class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        public SayaMusicTrack(string title)
+        {
+            Random random = new Random();
+            id = random.Next(10000, 99999);
+            this.title = title;
+            playCount = 0;
+        }
+
+        public void IncreasePlayCount(int count)
+        {
+            playCount += count;
+        }
+
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+        }
+    }
+}
